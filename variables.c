971d0d6b94c82d9bc0d6af60f3271e4a5bbe2524
ram-ugale-dev <ramugale4452@gmail.com>
2026-09-18 11:33:23 +0530

    learn variables

---
+++ variables.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int age = 20;
+	float height = 5.8;
+	char grade = 'A';
+
+
+	printf("Age = %d\n",age);
+	printf("Height = %f\n", height);
+	printf("Grade = %c\n",grade);
+
+
+	return 0;
+}
